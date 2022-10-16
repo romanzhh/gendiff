ed46545c0f1abde0c60b7a31b7938599be897602
roman <storkanderon222@gmail.com>
2022-10-16 23:25:55 +0300

    3rd commit

--- src/getInfo.js
+++ src/getInfo.js
@@ -3,10 +3,11 @@ export const program = new Command();
 
 program
   .name('gendiff')
+  .arguments('<filepath1> <filepath2>')
   .description('Compares two configuration files and shows a difference.')
-  .version('0.1.0', '-V, --version')
-
-program.command('help', { hidden: true })
+  .option('-V, --version', 'output the version number')
+  .option('-f, --format <type>', 'output format')
 
 program.parse();
+
 

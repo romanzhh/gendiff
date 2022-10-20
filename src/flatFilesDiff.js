f503619ffc2f2e1c66a42661c6ba40ebc31e590d
roman <storkanderon222@gmail.com>
2022-10-20 23:36:02 +0300

    lint files

--- src/flatFilesDiff.js
+++ src/flatFilesDiff.js
@@ -1,15 +1,13 @@
-export default (keys, f1, f2) => {
-    return keys.reduce((acc, key) => {
-        let result = acc;
-        if (f1[key] !== f2[key] && Object.hasOwn(f1, key)) {
-            result += `  - ${key}: ${f1[key]}\n`;
-          }
-        if (f1[key] !== f2[key] && Object.hasOwn(f2, key)) {
-            result += `  + ${key}: ${f2[key]}\n`;
-        }
-        if (f1[key] === f2[key]) {
-            result += `    ${key}: ${f1[key]}\n`;
-        }
-        return result;
-    }, '{\n') + '}';
-};
+export default (keys, f1, f2) => `${keys.reduce((acc, key) => {
+  let result = acc;
+  if (f1[key] !== f2[key] && Object.hasOwn(f1, key)) {
+    result += `  - ${key}: ${f1[key]}\n`;
+  }
+  if (f1[key] !== f2[key] && Object.hasOwn(f2, key)) {
+    result += `  + ${key}: ${f2[key]}\n`;
+  }
+  if (f1[key] === f2[key]) {
+    result += `    ${key}: ${f1[key]}\n`;
+  }
+  return result;
+}, '{\n')}}`;

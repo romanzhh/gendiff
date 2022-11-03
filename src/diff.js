d906463f7e4dff6f76cc3e1337dfb16177e5b474
roman <storkanderon222@gmail.com>
2022-11-03 22:59:35 +0300

    reduce cognitive complexity

--- src/diff.js
+++ src/diff.js
@@ -5,16 +5,16 @@ import makeIndent from './formatters/makeIndent.js';
 
 const keys = (f1, f2) => _.sortBy(_.uniq(_.union(Object.keys(f1), Object.keys(f2))));
 const compare = (f1, f2) => `${keys(f1, f2).reduce((acc, key) => {
-  if ((f1[key] !== f2[key] && Object.hasOwn(f1, key) && !_.isObject(f1[key]))
-  || (f1[key] !== f2[key] && Object.hasOwn(f1, key) && !_.isObject(f2[key]))) {
+  if ((f1[key] !== f2[key] && Object.hasOwn(f1, key)
+  && (!_.isObject(f1[key]) || !_.isObject(f2[key])))) {
     acc += `  - ${key}: ${stylish(f1[key])}\n`;
   }
-  if ((f1[key] !== f2[key] && Object.hasOwn(f2, key) && !_.isObject(f1[key]))
-  || (f1[key] !== f2[key] && Object.hasOwn(f2, key) && !_.isObject(f2[key]))) {
+  if (f1[key] !== f2[key] && Object.hasOwn(f2, key)
+  && (!_.isObject(f1[key]) || !_.isObject(f2[key]))) {
     acc += `  + ${key}: ${stylish(f2[key])}\n`;
   }
   if (f1[key] === f2[key]) {
-    acc += `    ${key}: ${stylish(f1[key])}\n`;
+    acc += `    ${key}: ${f1[key]}\n`;
   }
   if (_.isObject(f1[key]) && _.isObject(f2[key])) {
     acc += `    ${key}: ${makeIndent(compare(f1[key], f2[key]))}\n`;

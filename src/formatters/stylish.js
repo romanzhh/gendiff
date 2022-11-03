dc99ef08929cebae65e6a63afcf53a3a1376d321
roman <storkanderon222@gmail.com>
2022-11-03 15:28:32 +0300

    delete spaces

--- src/formatters/stylish.js
+++ src/formatters/stylish.js
@@ -4,5 +4,5 @@ export default (file) => {
   const newFile = JSON.stringify(file, null, 4);
   const unquoted = newFile.replaceAll('"', '');
   const result = makeIndent(unquoted);
-  return result.replaceAll(',', ' ');
+  return result.replaceAll(',', '').trim();
 };

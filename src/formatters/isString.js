86ae48011e098a5dd652c12104d030e06bfa730e
roman <storkanderon222@gmail.com>
2022-11-04 01:11:16 +0300

    update plain format

--- src/formatters/isString.js
+++ src/formatters/isString.js
@@ -2,5 +2,8 @@ export default (data) => {
   if (typeof data === 'string') {
     return `'${data}'`;
   }
+  if (typeof data === 'object' && data !== null) {
+    return '[complex value]';
+  }
   return data;
 };
--- src/plainDiff.js
+++ src/plainDiff.js
@@ -3,30 +3,37 @@ import _ from 'lodash';
 import dataType from './formatters/isString.js';
 
 const compare = (node1, node2) => {
-  const iter = (f1, f2) => {
+  const iter = (f1, f2, path) => {
     const keys = _.sortBy(_.uniq(_.union(Object.keys(f1), Object.keys(f2))));
-    return keys.reduce((acc, key) => {
+    const filter = keys.filter((key) => f1[key] !== f2[key]);
+    return filter.map((key) => {
+      const fullPath = (path === '') ? `${key}` : `${path}.${key}`;
       if (_.isObject(f2[key]) && !Object.hasOwn(f1, key)) {
-        acc += `Property '${key}' was added with value: [complex value]\n`;
-      } else if (_.isObject(f1[key]) && !Object.hasOwn(f2, key)) {
-        acc += `Property '${key}' was removed\n`;
-      } else if (_.isObject(f1[key]) && !_.isObject(f2[key])) {
-        acc += `Property '${key}' was updated. From [complex value] to ${dataType(f2[key])}`;
-      } else if (_.isObject(f1[key]) && _.isObject(f2[key])) {
-        acc += `${compare(f1[key], f2[key])}\n`;
+        return `Property '${fullPath}' was added with value: ${dataType(f2[key])}`;
+      }
+      if (_.isObject(f1[key]) && !Object.hasOwn(f2, key)) {
+        return `Property '${fullPath}' was removed`;
+      }
+      if (_.isObject(f1[key]) && !_.isObject(f2[key])) {
+        return `Property '${fullPath}' was updated. From ${dataType(f1[key])} to ${dataType(f2[key])}`;
+      }
+      if (_.isObject(f1[key]) && _.isObject(f2[key])) {
+        return `${iter(f1[key], f2[key], fullPath)}`;
       }
       if (!_.isObject(f1[key]) && !Object.hasOwn(f2, key)) {
-        acc += `Property '${key}' was removed\n`;
-      } else if (!_.isObject(f2[key]) && !Object.hasOwn(f1, key)) {
-        acc += `Property '${key}' was added with value: ${dataType(f2[key])}\n`;
-      } else if (!_.isObject(f1[key]) && f1[key] !== f2[key]
-      && Object.hasOwn(f1, key) && Object.hasOwn(f2, key)) {
-        acc += `Property '${key}' was updated. From ${dataType(f1[key])} to ${dataType(f2[key])}\n`;
+        return `Property '${fullPath}' was removed`;
       }
-      return acc;
-    }, '').trim();
+      if (!_.isObject(f2[key]) && !Object.hasOwn(f1, key)) {
+        return `Property '${fullPath}' was added with value: ${dataType(f2[key])}`;
+      }
+      if (!_.isObject(f1[key]) && f1[key] !== f2[key]
+      && Object.hasOwn(f1, key) && Object.hasOwn(f2, key)) {
+        return `Property '${fullPath}' was updated. From ${dataType(f1[key])} to ${dataType(f2[key])}`;
+      }
+      return null;
+    }).join('\n');
   };
-  return iter(node1, node2);
+  return iter(node1, node2, '');
 };
 
 export default compare;

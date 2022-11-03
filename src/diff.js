60ecf2877065ac807fb6dab6adad0be0223f2d2c
roman <storkanderon222@gmail.com>
2022-11-03 22:49:27 +0300

    reduce cognitive complexity

--- src/diff.js
+++ src/diff.js
@@ -3,15 +3,14 @@ import _ from 'lodash';
 import stylish from './formatters/stylish.js';
 import makeIndent from './formatters/makeIndent.js';
 
-const compare = (f1, f2) => `${_.sortBy(_.uniq(_.union(Object.keys(f1), Object.keys(f2)))).reduce((acc, key) => {
-  if (f1[key] !== f2[key] && Object.hasOwn(f1, key) && !_.isObject(f2[key])) {
-    acc += `  - ${key}: ${stylish(f1[key])}\n`;
-  } else if (f1[key] !== f2[key] && Object.hasOwn(f1, key) && !_.isObject(f1[key])) {
+const keys = (f1, f2) => _.sortBy(_.uniq(_.union(Object.keys(f1), Object.keys(f2))));
+const compare = (f1, f2) => `${keys(f1, f2).reduce((acc, key) => {
+  if ((f1[key] !== f2[key] && Object.hasOwn(f1, key) && !_.isObject(f1[key]))
+  || (f1[key] !== f2[key] && Object.hasOwn(f1, key) && !_.isObject(f2[key]))) {
     acc += `  - ${key}: ${stylish(f1[key])}\n`;
   }
-  if (f1[key] !== f2[key] && Object.hasOwn(f2, key) && !_.isObject(f1[key])) {
-    acc += `  + ${key}: ${stylish(f2[key])}\n`;
-  } else if (f1[key] !== f2[key] && Object.hasOwn(f2, key) && !_.isObject(f2[key])) {
+  if ((f1[key] !== f2[key] && Object.hasOwn(f2, key) && !_.isObject(f1[key]))
+  || (f1[key] !== f2[key] && Object.hasOwn(f2, key) && !_.isObject(f2[key]))) {
     acc += `  + ${key}: ${stylish(f2[key])}\n`;
   }
   if (f1[key] === f2[key]) {
@@ -22,5 +21,4 @@ const compare = (f1, f2) => `${_.sortBy(_.uniq(_.union(Object.keys(f1), Object.k
   }
   return acc;
 }, '{\n')}}`;
-
 export default compare;
--- src/plainDiff.js
+++ src/plainDiff.js
@@ -2,28 +2,31 @@
 import _ from 'lodash';
 import dataType from './formatters/isString.js';
 
-const compare = (f1, f2) => {
-  const keys = _.sortBy(_.uniq(_.union(Object.keys(f1), Object.keys(f2))));
-  return keys.reduce((acc, key) => {
-    if (!_.isObject(f1[key]) && !Object.hasOwn(f2, key)) {
-      acc += `Property '${key}' was removed\n`;
-    } else if (!_.isObject(f2[key]) && !Object.hasOwn(f1, key)) {
-      acc += `Property '${key}' was added with value: ${dataType(f2[key])}\n`;
-    } else if (!_.isObject(f1[key]) && f1[key] !== f2[key]
-    && Object.hasOwn(f1, key) && Object.hasOwn(f2, key)) {
-      acc += `Property '${key}' was updated. From ${dataType(f1[key])} to ${dataType(f2[key])}\n`;
-    }
-    if (_.isObject(f2[key]) && !Object.hasOwn(f1, key)) {
-      acc += `Property '${key}' was added with value: [complex value]\n`;
-    } else if (_.isObject(f1[key]) && !Object.hasOwn(f2, key)) {
-      acc += `Property '${key}' was removed\n`;
-    } else if (_.isObject(f1[key]) && !_.isObject(f2[key])) {
-      acc += `Property '${key}' was updated. From [complex value] to ${dataType(f2[key])}`;
-    } else if (_.isObject(f1[key]) && Object.hasOwn(f2, key)) {
-      acc += `${compare(f1[key], f2[key])}\n`;
-    }
-    return acc;
-  }, '').trim();
+const compare = (node1, node2) => {
+  const iter = (f1, f2) => {
+    const keys = _.sortBy(_.uniq(_.union(Object.keys(f1), Object.keys(f2))));
+    return keys.reduce((acc, key) => {
+      if (_.isObject(f2[key]) && !Object.hasOwn(f1, key)) {
+        acc += `Property '${key}' was added with value: [complex value]\n`;
+      } else if (_.isObject(f1[key]) && !Object.hasOwn(f2, key)) {
+        acc += `Property '${key}' was removed\n`;
+      } else if (_.isObject(f1[key]) && !_.isObject(f2[key])) {
+        acc += `Property '${key}' was updated. From [complex value] to ${dataType(f2[key])}`;
+      } else if (_.isObject(f1[key]) && _.isObject(f2[key])) {
+        acc += `${compare(f1[key], f2[key])}\n`;
+      }
+      if (!_.isObject(f1[key]) && !Object.hasOwn(f2, key)) {
+        acc += `Property '${key}' was removed\n`;
+      } else if (!_.isObject(f2[key]) && !Object.hasOwn(f1, key)) {
+        acc += `Property '${key}' was added with value: ${dataType(f2[key])}\n`;
+      } else if (!_.isObject(f1[key]) && f1[key] !== f2[key]
+      && Object.hasOwn(f1, key) && Object.hasOwn(f2, key)) {
+        acc += `Property '${key}' was updated. From ${dataType(f1[key])} to ${dataType(f2[key])}\n`;
+      }
+      return acc;
+    }, '').trim();
+  };
+  return iter(node1, node2);
 };
 
 export default compare;

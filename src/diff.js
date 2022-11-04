ad07a0ec772665665e9c4baf1d499ecb65807dc8
roman <storkanderon222@gmail.com>
2022-11-04 14:45:59 +0300

    remove mutation

--- src/diff.js
+++ src/diff.js
@@ -1,27 +1,28 @@
+/* eslint-disable no-param-reassign */
 import _ from 'lodash';
 import stylish from './formatters/stylish.js';
 import makeIndent from './formatters/makeIndent.js';
 
-const getKeys = (f1, f2) => _.sortBy(_.uniq(_.union(_.keys(f1), _.keys(f2))));
+const clone = (f1, f2) => _.union(_.keys(_.cloneDeep(f1)), _.keys(_.cloneDeep(f2)));
+const getKeys = (f1, f2) => _.sortBy(_.uniq(clone(f1, f2)));
 const compare = (f1, f2) => {
   const keys = getKeys(f1, f2);
   return `${keys.reduce((acc, key) => {
-    let result = acc;
     if ((f1[key] !== f2[key] && Object.hasOwn(f1, key)
     && (!_.isObject(f1[key]) || !_.isObject(f2[key])))) {
-      result += `  - ${key}: ${stylish(f1[key])}\n`;
+      acc += `  - ${key}: ${stylish(f1[key])}\n`;
     }
     if (f1[key] !== f2[key] && Object.hasOwn(f2, key)
     && (!_.isObject(f1[key]) || !_.isObject(f2[key]))) {
-      result += `  + ${key}: ${stylish(f2[key])}\n`;
+      acc += `  + ${key}: ${stylish(f2[key])}\n`;
     }
     if (f1[key] === f2[key]) {
-      result += `    ${key}: ${f1[key]}\n`;
+      acc += `    ${key}: ${f1[key]}\n`;
     }
     if (_.isObject(f1[key]) && _.isObject(f2[key])) {
-      result += `    ${key}: ${makeIndent(compare(f1[key], f2[key]))}\n`;
+      acc += `    ${key}: ${makeIndent(compare(f1[key], f2[key]))}\n`;
     }
-    return result;
+    return acc;
   }, '{\n')}}`;
 };
 export default compare;
